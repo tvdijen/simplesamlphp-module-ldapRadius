df0332c870e34500fd2ededc396ff9cd1a5f62c3
Tim van Dijen <tvdijen@gmail.com>
2022-11-20 22:52:19 +0100

    Add some more parameters to configure

---
+++ src/Auth/Source/LdapRadius.php
@@ -0,0 +1,195 @@
+<?php
+
+declare(strict_types=1);
+
+namespace SimpleSAML\Module\ldapRadius\Auth\Source;
+
+use Exception;
+use SimpleSAML\Assert\Assert;
+use SimpleSAML\Auth;
+use SimpleSAML\Configuration;
+use SimpleSAML\Error;
+use SimpleSAML\Logger;
+use SimpleSAML\Module;
+use SimpleSAML\Module\ldap\Auth\Source\Ldap;
+use SimpleSAML\Module\radius\Auth\Source\Radius;
+use SimpleSAML\Utils;
+
+use function array_keys;
+use function array_merge;
+use function in_array;
+use function sprintf;
+
+/**
+ * Class for username/password/otp authentication.
+ *
+ * @package tvdijen/simplesamlphp-module-ldapRadius
+ */
+final class LdapRadius extends Auth\Source
+{
+    /**
+     * The string used to identify our states.
+     */
+    public const STAGEID = '\SimpleSAML\Module\ldapRadius\Auth\Source\LdapRadius.state';
+
+    /**
+     * The key of the AuthId field in the state.
+     */
+    public const AUTHID = '\SimpleSAML\Module\ldapRadius\Auth\Source\LdapRadius.AuthId';
+
+    /**
+     * The primary authentication source to authenticate with.
+     */
+    private string $primarySource;
+
+    /**
+     * The secondary authentication source to authenticate with.
+     */
+    private string $secondarySource;
+
+    /**
+     * The input type for the username-field. Can be 'text' or 'email'.
+     */
+    private string $usernameTypeHint = 'text';
+    private ?string $usernamePattern;
+    private ?int $passwordMinLength;
+    private ?string $otpInputMode;
+    private ?string $otpPattern;
+
+    /**
+     * Constructor for this authentication source.
+     *
+     * @param array $info  Information about this authentication source.
+     * @param array &$config  Configuration for this authentication source.
+     */
+    public function __construct(array $info, array &$config)
+    {
+        // Call the parent constructor first, as required by the interface
+        parent::__construct($info, $config);
+
+        Assert::keyExists($config, 'primary');
+        Assert::keyExists($config, 'secondary');
+        Assert::stringNotEmpty($config['primary']);
+        Assert::stringNotEmpty($config['secondary']);
+
+        $this->primarySource = $config['primary'];
+        $this->secondarySource = $config['secondary'];
+
+        $this->usernameTypeHint = $config['usernameTypeHint'] ?? 'text';
+        Assert::oneOf($this->usernameTypeHint, ['text', 'email']);
+
+        $this->usernamePattern = $config['usernamePattern'] ?? null;
+        $this->passwordMinLength = $config['passwordMinLength'] ?? null;
+        $this->otpInputMode = $config['otpInputMode'] ?? null;
+        $this->otpPattern = $config['otpPattern'] ?? null;
+
+        $authsources = Configuration::getConfig('authsources.php')->toArray();
+        Assert::keyExists($authsources, $this->primarySource);
+        Assert::keyExists($authsources, $this->secondarySource);
+    }
+
+
+    /**
+     * Process a request.
+     *
+     * If an authentication source returns from this function, it is assumed to have
+     * authenticated the user, and should have set elements in $state with the attributes
+     * of the user.
+     *
+     * If the authentication process requires additional steps which make it impossible to
+     * complete before returning from this function, the authentication source should
+     * save the state, and at a later stage, load the state, update it with the authentication
+     * information about the user, and call completeAuth with the state array.
+     *
+     * @param array &$state Information about the current authentication.
+     */
+    public function authenticate(array &$state): void
+    {
+        $state[self::AUTHID] = $this->authId;
+        $state['ldapRadius:usernameTypeHint'] = $this->usernameTypeHint;
+        $state['ldapRadius:usernamePattern'] = $this->usernamePattern;
+        $state['ldapRadius:passwordMinLength'] = $this->passwordMinLength;
+        $state['ldapRadius:otpInputMode'] = $this->otpInputMode;
+        $state['ldapRadius:otpPattern'] = $this->otpPattern;
+
+        // Save the $state array, so that we can restore if after a redirect
+        $id = Auth\State::saveState($state, self::STAGEID);
+
+        /* Redirect to the select source page. We include the identifier of the
+         * saved state array as a parameter to the login form
+         */
+        $url = Module::getModuleURL('ldapRadius/login');
+        $params = ['AuthState' => $id];
+
+        $httpUtils = new Utils\HTTP();
+        $httpUtils->redirectTrustedURL($url, $params);
+    }
+
+
+    /**
+     * Handle login request.
+     *
+     * This function is used by the login form (ldapRadius/loginuserpassotp) when the user
+     * enters a username, password and otp. On success, it will not return. On wrong
+     * username/password failure, otp failure and other errors, it will throw an exception.
+     *
+     * @param string $authStateId  The identifier of the authentication state.
+     * @param string $username  The username the user wrote.
+     * @param string $password  The password the user wrote.
+     * @param string $otp  The otp the user wrote.
+     */
+    public static function handleLogin(array $state, string $username, string $password, string $otp): void
+    {
+        // Retrieve the authentication source we are executing.
+        Assert::keyExists($state, self::AUTHID);
+
+        //** @var \SimpleSAML\Auth\Source|null $source
+        $source = Auth\Source::getById($state[self::AUTHID]);
+        if ($source === null) {
+            throw new Exception(sprintf("Could not find authentication source with id '%s'", $state[self::AUTHID]));
+        }
+
+        // Attempt to log in
+        try {
+            $attributes = $source->login($username, $password, $otp);
+        } catch (Exception $e) {
+            Logger::stats(sprintf("Unsuccessful login attempt from '%s'.", $_SERVER['REMOTE_ADDR']));
+            throw $e;
+        }
+        Logger::stats(sprintf("User '%s' successfully authenticated from '%s'.", $username, $_SERVER['REMOTE_ADDR']));
+
+        // Save the attributes we received from the login-function in the $state-array
+        $state['Attributes'] = $attributes;
+
+        // Return control to SimpleSAMLphp after successful authentication.
+        parent::completeAuth($state);
+    }
+
+
+    public function login(string $username, string $password, string $otp): array
+    {
+        $primarySource = Auth\Source::getById($this->primarySource);
+
+        $authsources = Configuration::getConfig('authsources.php')->toArray();
+        $ldap = new class (['AuthId' => $this->primarySource], $authsources[$this->primarySource]) extends Ldap
+        {
+            public function loginOverload(string $username, string $password): array
+            {
+                return $this->login($username, $password);
+            }
+        };
+
+        $ldapAttributes = $ldap->loginOverload($username, $password);
+
+        $radius = new class(['AuthId' => $this->secondarySource], $authsources[$this->secondarySource]) extends Radius
+        {
+            public function loginOverload(string $username, string $otp): array
+            {
+                return $this->login($username, $otp);
+            }
+        };
+
+        $radiusAttributes = $radius->loginOverload($username, $otp);
+        return array_merge($ldapAttributes, $radiusAttributes);
+    }
+}
---
+++ src/Controller/Login.php
@@ -0,0 +1,132 @@
+<?php
+
+declare(strict_types=1);
+
+namespace SimpleSAML\Module\ldapRadius\Controller;
+
+use Exception;
+use SimpleSAML\Assert\Assert;
+use SimpleSAML\Auth;
+use SimpleSAML\Configuration;
+use SimpleSAML\Error;
+use SimpleSAML\Module\ldapRadius\Auth\Source\LdapRadius;
+use SimpleSAML\XHTML\Template;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+
+/**
+ * Controller class for the ldapRadius module.
+ *
+ * This class serves the different views available in the module.
+ *
+ * @package tvdijen/simplesamlphp-module-ldapRadius
+ */
+class Login
+{
+    /** @var \SimpleSAML\Configuration */
+    protected Configuration $config;
+
+    /**
+     * @var \SimpleSAML\Auth\State|string
+     * @psalm-var \SimpleSAML\Auth\State|class-string
+     */
+    protected $authState = Auth\State::class;
+
+
+    /**
+     * Controller constructor.
+     *
+     * It initializes the global configuration for the controllers implemented here.
+     *
+     * @param \SimpleSAML\Configuration $config The configuration to use by the controllers.
+     *
+     * @throws \Exception
+     */
+    public function __construct(
+        Configuration $config
+    ) {
+        $this->config = $config;
+    }
+
+
+    /**
+     * Inject the \SimpleSAML\Auth\State dependency.
+     *
+     * @param \SimpleSAML\Auth\State $authState
+     */
+    public function setAuthState(Auth\State $authState): void
+    {
+        $this->authState = $authState;
+    }
+
+
+    /**
+     * This page shows a username/password/otp login form, and passes information from it
+     * to a primary and then a secondary auth source.
+     *
+     * @param \Symfony\Component\HttpFoundation\Request $request
+     * @return \SimpleSAML\XHTML\Template|\SimpleSAML\HTTP\RunnableResponse
+     */
+    public function login(Request $request): Response
+    {
+        // Retrieve the authentication state
+        if (!$request->query->has('AuthState')) {
+            throw new Error\BadRequest('Missing AuthState parameter.');
+        }
+        $authStateId = $request->query->get('AuthState');
+
+        $state = $this->authState::loadState($authStateId, LdapRadius::STAGEID);
+        $source = Auth\Source::getById($state[LdapRadius::AUTHID]);
+
+        $errorCode = null;
+        $errorParams = null;
+
+        if (isset($state['error'])) {
+            $errorCode = $state['error']['code'];
+            $errorParams = $state['error']['params'];
+        }
+
+        if ($request->server->get('REQUEST_METHOD') === 'POST' &&
+            $request->request->has('username') &&
+            $request->request->has('password') &&
+            $request->request->has('otp')
+        ) {
+            $username = $request->get('username');
+            $password = $request->get('password');
+            $otp = $request->get('otp');
+
+            try {
+                $source->handleLogin($state, $username, $password, $otp);
+            } catch (Error\Error $e) {
+                // Login failed. Extract error code and parameters, to display the error
+                $errorCode = $e->getErrorCode();
+                $errorParams = $e->getParameters();
+                $state['error'] = [
+                    'code' => $errorCode,
+                    'params' => $errorParams
+                ];
+                $authStateId = $this->authState::saveState($state, $source::STAGEID);
+            }
+
+            if (isset($state['error'])) {
+                unset($state['error']);
+            }
+        }
+
+        // Build template
+        $t = new Template($this->config, 'ldapRadius:login.twig');
+
+        $t->data['AuthState'] = $this->authState::saveState($state, LdapRadius::STAGEID);
+        $t->data['errorcode'] = $errorCode;
+        $t->data['errorcodes'] = Error\ErrorCodes::getAllErrorCodeMessages();
+        $t->data['errorparams'] = $errorParams;
+
+        $t->data['usernameTypeHint'] = $state['ldapRadius:usernameTypeHint'];
+        $t->data['usernamePattern'] = $state['ldapRadius:usernamePattern'];
+        $t->data['passwordMinLength'] = $state['ldapRadius:passwordMinLength'];
+        $t->data['otpInputMode'] = $state['ldapRadius:otpInputMode'];
+        $t->data['otpPattern'] = $state['ldapRadius:otpPattern'];
+
+        return $t;
+    }
+}
